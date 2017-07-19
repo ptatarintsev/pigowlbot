2b8d75c802f72edb46ceaecde7210de23440d273
Pavel <Pavel@MacBook-Pro-Anna.local>
2017-07-19 14:06:32 +0300

    Refactoring, added getdownloads command

--- server.go
+++ server.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"pigowlbot/token"
 	"strings"
+	"time"
 
 	"gopkg.in/telegram-bot-api.v4"
 )
@@ -33,15 +34,24 @@ type PackResponse struct {
 	Count int  `json:"count"`
 }
 
+type PackStatResponse struct {
+	time.Time  `json:"timestamp"`
+	ID int  `json:"id"`
+}
+
 type GetPacksResponse struct {
 	Packs  []PackResponse `json:"packs"`
+}
+
+type GetPacksStatResponse struct {
+	PacksStat  []PackStatResponse
 }
 
 func MainHandler(resp http.ResponseWriter, _ *http.Request) {
 	resp.Write([]byte("Hi there! I'm PigowlTestBot!"))
 }
 
-func main() {
+func getPacksResponse() *GetPacksResponse {
 	url := "http://pigowl.com:8080/getPacks"
 
 	res, err := http.Get(url)
@@ -49,16 +59,43 @@ func main() {
 		log.Fatal(err)
 	}
 	defer res.Body.Close()
-	
+
 	response := new(GetPacksResponse) 
 	json.NewDecoder(res.Body).Decode(response)
+	return response
+}
+
+func getPackages() string {
+	response := getPacksResponse()
 
 	var parts []string
 	for _, pack := range response.Packs {
 		parts = append(parts, pack.Pack.Name)
 	}
-	log.Printf("%s", strings.Join(parts," "))
+	return strings.Join(parts,"\n")
+}
 
+func getDownloads() string {
+	url := "http://pigowl.com:8080/getPacksStat"
+
+	res, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	response := new(GetPacksStatResponse) 
+	json.NewDecoder(res.Body).Decode(response)
+
+	//var parts []string
+	//for _, pack := range response.Packs {
+	//	parts = append(parts, pack.Pack.Name)
+	//}
+	//return strings.Join(parts,"\n")
+	return url
+}
+
+func main() {
 	bot, err := tgbotapi.NewBotAPI(token.BotToken)
 	if err != nil {
 		log.Fatal(err)
@@ -68,31 +105,23 @@ func main() {
 
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
-	//_, err = bot.SetWebhook(tgbotapi.NewWebhookWithCert("https://www.pigowl.com:8443/"+bot.Token, "fullchain.pem"))
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//u := tgbotapi.NewUpdate(0)
-	//u.Timeout = 60
-
-	//updates, err := bot.GetUpdatesChan(u)
 	updates := bot.ListenForWebhook("/" + token.BotToken)
 
 	http.HandleFunc("/", MainHandler)
 	go http.ListenAndServe(":"+os.Getenv("PORT"), nil)
-	//updates := bot.ListenForWebhook("/" + bot.Token)
-	//go http.ListenAndServeTLS(":8443", "fullchain.pem", "privkey.pem", nil)
 
 	for update := range updates {
 		command := update.Message.Command()
 		if command == "" {
 			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Hi there!")
-			//msg.ReplyToMessageID = update.Message.MessageID
 			bot.Send(msg)
 		} else {
 			switch command {
 				case "getpackages":
-					msg := tgbotapi.NewMessage(update.Message.Chat.ID, strings.Join(parts,"\n"))
+					msg := tgbotapi.NewMessage(update.Message.Chat.ID, getPackages())
+					bot.Send(msg)
+				case "getdownloads":
+					msg := tgbotapi.NewMessage(update.Message.Chat.ID, getDownloads())
 					bot.Send(msg)
 				}
 		}
