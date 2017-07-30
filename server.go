8e2c0872e97cad426c49dffb28042b7e194d54ec
Tatarintsev Pavel <pavel.tatarincev@gmail.com>
2017-07-30 12:40:35 +0300

    Merged with origin pigowl bot

--- server.go
+++ server.go
@@ -26,7 +26,7 @@ func getPackages() string {
 	for _, pack := range response.Packs {
 		parts = append(parts, pack.Pack.Name)
 	}
-	return strings.Join(parts,"\n")
+	return strings.Join(parts, "\n")
 }
 
 func getPackagesName() map[int]string {
@@ -42,10 +42,10 @@ func getPackagesName() map[int]string {
 func formatDownloadsMessage(sortedMap *sort.SortedMap) string {
 	var result []string
 	for _, v := range sortedMap.Keys {
-		result = append(result, v + ", " + strconv.Itoa(sortedMap.Original[v]))
+		result = append(result, v+", "+strconv.Itoa(sortedMap.Original[v]))
 	}
 	if len(result) > 0 {
-		return strings.Join(result,"\n")
+		return strings.Join(result, "\n")
 	}
 	return "There were not any downloads :'("
 }
@@ -53,37 +53,49 @@ func formatDownloadsMessage(sortedMap *sort.SortedMap) string {
 func formatDiffDownloadsMessage(updatedMap *sort.SortedMap, dailyMap *sort.SortedMap) string {
 	var result []string
 	for _, v := range updatedMap.Keys {
-		result = append(result, v + ", " + strconv.Itoa(updatedMap.Original[v]) + " (" + strconv.Itoa(dailyMap.Original[v]) + ")")
+		result = append(result, v+", "+strconv.Itoa(updatedMap.Original[v])+" ("+strconv.Itoa(dailyMap.Original[v])+")")
 	}
 	if len(result) > 0 {
-		return strings.Join(result,"\n")
+		return strings.Join(result, "\n")
 	}
 	return "There were not any downloads :'("
 }
 
 func getDownloads(period int64) *sort.SortedMap {
-	packageIdNameMap := getPackagesName()
+	packageIDNameMap := getPackagesName()
 	packsStatResponse := api.GetPackagesStatistics()
 
 	downloadsMap := make(map[string]int)
 	for _, packStat := range packsStatResponse.PacksStat {
 		if packStat.Timestamp >= period {
-			downloadsMap[packageIdNameMap[packStat.ID]]++
+			downloadsMap[packageIDNameMap[packStat.ID]]++
 		}
 	}
 
 	return sort.SortedKeys(downloadsMap)
 }
 
-func getRealGames(period int64) int {
-	realGamesResponse := api.GetRealGames()
-	var result int
-	for _, realGame := range realGamesResponse.Games {
-		if realGame.Timestamp >= period {
-			result++
+func timer(channel chan int) {
+	for {
+		time.Sleep(10 * time.Minute)
+		//time.Sleep(time.Second * 5)
+		channel <- 0
+	}
+}
+
+func subscribe(bot *tgbotapi.BotAPI, chatID int64) {
+	lastDownloadsTimestamp := time.Now().Unix()
+	timerChannel := make(chan int)
+	go timer(timerChannel)
+	for _ = range timerChannel {
+		downloads := getDownloads(lastDownloadsTimestamp)
+		lastDownloadsTimestamp = time.Now().Unix()
+
+		if len(downloads.Keys) > 0 {
+			msg := tgbotapi.NewMessage(chatID, formatDiffDownloadsMessage(downloads, getDownloads(time.Now().Truncate(24*time.Hour).Unix())))
+			bot.Send(msg)
 		}
 	}
-	return result
 }
 
 func main() {
@@ -92,14 +104,14 @@ func main() {
 		log.Fatal(err)
 	}
 
-	bot.Debug = true
-
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
 	updates := bot.ListenForWebhook("/" + token.BotToken)
 
 	http.HandleFunc("/", MainHandler)
 	go http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+
+	subscribers := make(map[int64]bool)
 
 	for update := range updates {
 		command := update.Message.Command()
@@ -112,24 +124,21 @@ func main() {
 					msg := tgbotapi.NewMessage(update.Message.Chat.ID, getPackages())
 					bot.Send(msg)
 				case "getweeklydownloads":
-					msg := tgbotapi.NewMessage(update.Message.Chat.ID, formatDownloadsMessage(getDownloads(time.Now().Add(-7*24*time.Hour).Truncate(24 * time.Hour).Unix())))
+					msg := tgbotapi.NewMessage(update.Message.Chat.ID, formatDownloadsMessage(getDownloads(time.Now().Add(-7*24*time.Hour).Truncate(24*time.Hour).Unix())))
 					bot.Send(msg)
 				case "getdailydownloads":
-					msg := tgbotapi.NewMessage(update.Message.Chat.ID, formatDownloadsMessage(getDownloads(time.Now().Truncate(24 * time.Hour).Unix())))
-					bot.Send(msg)
-				case "getdailyrealgames":
-					msg := tgbotapi.NewMessage(update.Message.Chat.ID, strconv.Itoa(getRealGames(time.Now().Truncate(24 * time.Hour).Unix())))
-					bot.Send(msg)
-				case "getweeklyrealgames":
-					msg := tgbotapi.NewMessage(update.Message.Chat.ID, strconv.Itoa(getRealGames(time.Now().Add(-7*24*time.Hour).Truncate(24 * time.Hour).Unix())))
+					msg := tgbotapi.NewMessage(update.Message.Chat.ID, formatDownloadsMessage(getDownloads(time.Now().Truncate(24*time.Hour).Unix())))
 					bot.Send(msg)
 				case "getalldownloads":
 					msg := tgbotapi.NewMessage(update.Message.Chat.ID, formatDownloadsMessage(getDownloads(0)))
 					bot.Send(msg)
-				case "getdiffdownloads":
-					msg := tgbotapi.NewMessage(update.Message.Chat.ID, formatDiffDownloadsMessage(getDownloads(time.Now().Truncate(24 * time.Hour).Unix()),	getDownloads(time.Now().Add(-7*24*time.Hour).Truncate(24 * time.Hour).Unix())))
-					bot.Send(msg)
-				}
+				case "subscribe":
+					_, exist := subscribers[update.Message.Chat.ID]
+					if !exist {
+						go subscribe(bot, update.Message.Chat.ID)
+						subscribers[update.Message.Chat.ID] = true
+					}
+			}
 		}
 	}
 }
